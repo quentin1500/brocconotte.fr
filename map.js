ef75ffa292342d2ce808d028f5178426d2404c47
Quentin LAGONOTTE <Quentin.LAGONOTTE@nantesmetropole.fr>
2026-01-09 16:37:26 +0100

    ajout de la carte de suivi et de nouveaux sponsors

---
+++ map.js
@@ -0,0 +1,28 @@
+// Coordonnées GPS (exemple : Paris)
+const twingoLocation = {
+  lat: 47.209701,
+  lng: -1.571254
+};
+
+// Initialisation de la carte
+const map = L.map('map').setView(
+  [twingoLocation.lat, twingoLocation.lng],
+  15
+);
+
+// Fond de carte OpenStreetMap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap'
+}).addTo(map);
+
+const twingoIcon = L.icon({
+  iconUrl: './assets/pixel_art_36x36.png',
+  iconSize: [40, 40]
+});
+
+// Marqueur twingo
+const marker = L.marker([twingoLocation.lat, twingoLocation.lng], { icon: twingoIcon })
+  .addTo(map)
+  .bindPopup('🏎️ Brocconotte Twingo')
+  .openPopup();
+
